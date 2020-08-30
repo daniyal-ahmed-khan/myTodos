2db04ccbfe02fb9cfc9cb345548f4d58073cf9ee
daniyal-ahmed-khan <infusion_turbo360@hotmail.com>
2020-08-30 13:38:11 +0500

    second commit

--- screens/home.js
+++ screens/home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Platform, TouchableWithoutFeedback, Alert, Keyboard,Button, Text, View, FlatList } from 'react-native';
+import { StyleSheet, ActionButton, TouchableWithoutFeedback, Alert, Keyboard, Button, Text, View, FlatList } from 'react-native';
 import firebase from 'react-native-firebase';
 import TodoItem from '../components/TodoItem';
 import AddTodo from '../components/AddTodo';
@@ -7,8 +7,8 @@ import AddTodo from '../components/AddTodo';
 export default class Home extends React.Component {
 
   state = {
-    todos : [
-      {text : '', key : ''},
+    todos: [
+      { text: '', key: '' },
       // {text: 'repair system', key:'1'},
       // {text: 'install updates', key:'2'},
       // {text: 'start working', key:'3'},
@@ -21,116 +21,131 @@ export default class Home extends React.Component {
       // {text: 'Record Response', key:'10'},
     ],
     currentUser: null,
-    userEmail : '',
+    userEmail: '',
   }
 
-componentDidMount() {
-  const { currentUser } = firebase.auth();
-  const email = currentUser.email;
+  async componentDidMount() {
+    const { currentUser } = await firebase.auth();
+    const email = currentUser.email;
 
-  this.setState({ currentUser}); 
-  this.setState({userEmail : email});
-  console.log('email =' + this.state.userEmail);
-
-  firebase.firestore()
-    .collection('user1')
-    .doc(currentUser.uid)
-    .get()
-    .then((doc) => { 
-      const data = doc.data().todos;
-      const newArray = [];
-      data.map((item)=>{
-        newArray.push({text : item, key : Math.random().toString()});
-      })
-      this.setState({todos : newArray})   
-    }) 
+    this.setState({ currentUser });
+    this.setState({ userEmail: email });
    
-}
 
-signOutUser = async () => {
-  try {
-      const {currentUser} = await firebase.auth().signOut();
-      this.setState({currentUser});
-      this.props.navigation.navigate('SignUp');
-  } catch (e) {
-      console.log(e);
+    firebase.firestore()
+      .collection('user1')
+      .doc(currentUser.uid)
+      .get()
+      .then((doc) => {
+        const data = doc.data().todos;
+        const newArray = [];
+        data.map((item) => {
+          newArray.push({ text: item, key: Math.random().toString() });
+        })
+        this.setState({ todos: newArray })
+      })
+
   }
-}
-pressHandler = (key) => {
-  
+
+  signOutUser = async () => {
+    try {
+      const { currentUser } = await firebase.auth().signOut();
+      this.setState({ currentUser });
+      this.props.navigation.navigate('SignUp');
+    } catch (e) {
+      console.log(e);
+    }
+  }
+  pressHandler = (key) => {
+
     const prevTodos = this.state.todos;
 
     const newList = prevTodos.filter(todo => todo.key != key);
 
-  this.setState({todos : newList});
-}
+    this.setState({ todos: newList });
+  }
 
-submitHandler =(text) => {
-  if (text.length > 3) {
+  submitHandler = (text) => {
+    if (text.length > 3) {
 
       const prevTodos = this.state.todos;
       const newList = [
-        {text: text, key: Math.random().toString()},
+        { text: text, key: Math.random().toString() },
         ...prevTodos
       ];
+      this.setState({ todos: newList });
+
+
+
+
       const todoText = [];
-      newList.map( (item) => {
+      newList.map((item) => {
         todoText.push(item.text);
       });
       console.log(todoText);
-      this.setState({todos : newList});
+      
+
+
+
+
+      firebase.firestore().collection("user1").doc(this.state.currentUser.uid).update({
+
+
+        todos: todoText
+      })
 
       // firebase.database().ref("user1/").
       // set({
       //   todos: todoText,
       // }).then(() => console.log('Data set.'));;
-  } 
-  else {
-    Alert.alert('Naah!!', 'Todos Length Over 3 Chars.', [
-      {text: 'Got it!', onPress: () => {}},
-    ])
+    }
+    else {
+      Alert.alert('Naah!!', 'Todos Length Over 3 Chars.', [
+        { text: 'Got it!', onPress: () => { } },
+      ])
+    }
   }
-}
 
-render() {
-  const { currentUser } = this.state;
-  
-  return (
-    
-    <TouchableWithoutFeedback onPress={() => {Keyboard.dismiss();}}>
-       <View style={styles.container}>
+  render() {
+    const { currentUser } = this.state;
 
-         <View style= {styles.content}>
+    return (
 
-           <Text style= {{
-                borderBottomWidth: 2,
-                borderBottomColor: '#ddd',
-                fontSize: 40,
-                paddingTop: 10,
-                paddingLeft: 80,
-                fontWeight: 'bold',
-              }}>
-                My Todos
+      <TouchableWithoutFeedback onPress={() => { Keyboard.dismiss(); }}>
+        <View style={styles.container}>
+
+          <View style={styles.content}>
+            <Text>logged in as, {this.state.userEmail}</Text>
+
+            <Text style={{
+              borderBottomWidth: 2,
+              borderBottomColor: '#ddd',
+              fontSize: 40,
+              paddingTop: 10,
+              paddingLeft: 80,
+              fontWeight: 'bold',
+            }}>
+              My Todos
             </Text>
 
             <View style={styles.list}>
-              <FlatList  data={this.state.todos} renderItem={({item}) => (
+              <FlatList data={this.state.todos} renderItem={({ item }) => (
                 <TodoItem item={item} pressHandler={this.pressHandler} />
-                )}>
+              )}>
               </FlatList>
-            </View>    
+            </View>
             <AddTodo submitHandler={this.submitHandler} />
-            <Button  title="Logout" onPress={this.signOutUser}>Logout</Button>
-         </View>
-        
-       </View>
-     </TouchableWithoutFeedback>
+             <Button title="Logout" onPress={this.signOutUser}>Logout</Button> 
+          </View>
+
+        </View>
+      </TouchableWithoutFeedback>
       // <View style={styles.container}>
       //   <Text style={{fontSize: 20}}> Hi <Text style={{color:'#e93766', fontSize: 20}}> 
       //     {currentUser && currentUser.email}!
       //   </Text></Text>
-        
-       // <Button title="Logout" onPress={this.signOutUser}>Logout</Button>
+
+      // <Button title="Logout" onPress={this.signOutUser}>Logout</Button>
 
     )
   }
@@ -142,10 +157,10 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
   },
-  content : {
+  content: {
     flex: 1,
     paddingRight: 10,
-    paddingLeft: 20, 
+    paddingLeft: 20,
     paddingTop: 40,
     paddingBottom: 90
   },
@@ -153,8 +168,8 @@ const styles = StyleSheet.create({
     flex: 1,
     marginTop: 20,
   },
-  logout : {
-    width : 70,
+  logout: {
+    width: 70,
     height: 20,
     backgroundColor: 'red'
   }
